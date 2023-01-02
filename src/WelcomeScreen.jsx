0a3b0244d741333c944a0b7dca2a753d77aa8b99
Brian Smawley <briansmawley@gmail.com>
2023-01-02 14:53:22 -0500

    Changes to URIs

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -30,7 +30,7 @@ class="btn-text"
 </div>
 </div>
 <a
-href="https://ChefLuBu.github.io/meet/privacy.html"
+href="https://singular-ganache-fd70cd.netlify.app/privacy.html"
 rel="nofollow noopener"
 >
 Privacy policy
