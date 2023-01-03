cd2f5f0e02be02597e18af40dac0c960d56f961c
Brian Smawley <briansmawley@gmail.com>
2023-01-03 13:39:24 -0500

    Adding Scatter Grid

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import "./styles/nprogress.css";
 import { InfoAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 class App extends Component {
   state = {
@@ -16,7 +24,19 @@ class App extends Component {
     showWelcomeScreen: undefined,
   };
 
-  updateEvents = (location, NumberOfEvents) => {
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(", ").shift();
+      return { city, number };
+    });
+    return data;
+  };
+
+  updateEvents = (location, numberOfEvents) => {
     getEvents().then((events) => {
       const locationEvents =
         location === "all"
@@ -49,11 +69,14 @@ class App extends Component {
   }
 
   render() {
-      if (this.state.showWelcomeScreen === undefined){
+    const { locations, numberOfEvents } = this.state;
+    if (this.state.showWelcomeScreen === undefined) {
       return <div className="App" />;
     }
     return (
       <div className="App">
+        <h1>Meet App</h1>
+          <h4>Choose your nearest city</h4>
         {!navigator.onLine && (
           <InfoAlert
             className="alert-centered"
@@ -65,6 +88,32 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <NumberOfEvents updateEvents={this.updateEvents} />
+        <div className="App">
+          
+          <CitySearch updateEvents={this.updateEvents} locations={locations} />
+          <NumberOfEvents
+            updateEvents={this.updateEvents}
+            numberOfEvents={numberOfEvents}
+          />
+          <h4>Events in each city</h4>
+
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number of events" />            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+          <EventList events={this.state.events} />
+        </div>
         <EventList events={this.state.events} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
